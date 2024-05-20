7e7fbf4ca2a7fc06e354888453428cd31e3cdcb3
AlexMorskyi <Alex.capone35@gmail.com>
2024-05-20 16:24:09 +0200

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,7 +12,11 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice ('Hello from my custom JavaScript Action!');
+    const websiteUrl = `http://${bucket}.s3-webside-${bucketRegion}.amazonaws.com`;
+    core:core.setOutput('website-url', websiteUrl) // ::set-output
+
+
+
 }
 
 run();
